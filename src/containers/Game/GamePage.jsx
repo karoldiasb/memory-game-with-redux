ba5f7c411277ed7aa7b468da42c8ae427be4d590
Karoline Barreto <karolinedbarreto@gmail.com>
2022-01-05 10:24:03 -0300

    separa código em arquivos de acordo com arquitetura do redux

--- src/containers/Game/GamePage.jsx
+++ src/containers/Game/GamePage.jsx
@@ -3,6 +3,8 @@ import { connect } from "react-redux";
 
 import { Board, Card } from "../../components";
 
+import { selectCard } from "../../store/actions";
+
 const GamePage = ({ cards, onCardClick }) => (
   <Board>
     {cards.map((card) => (
@@ -11,7 +13,7 @@ const GamePage = ({ cards, onCardClick }) => (
         name={card.name}
         isActive={card.isActive}
         onClick={() => {
-          onCardClick(card);
+          onCardClick(card.key);
         }}
       />
     ))}
@@ -22,10 +24,16 @@ const mapStateToProps = (state) => ({
   cards: state.cards,
 });
 
-const mapDispatchToProps = (dispatch) => ({
-  onCardClick: (card) => {
-    dispatch({ type: "SELECT_CARD", key: card.key });
-  },
-});
+// const mapDispatchToProps = (dispatch) => ({
+//   onCardClick: (card) => {
+// o GamePage não deve saber como o redux funciona, por isso utiliza-se actions creators (selectCard)
+//     // dispatch({ type: "SELECT_CARD", key: card.key });
+//     dispatch(selectCard(card.key));
+//   },
+// });
+
+const mapDispatchToProps = {
+  onCardClick: selectCard,
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(GamePage);
